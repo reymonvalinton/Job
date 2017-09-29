56442a33f296c9e781ea4f1685ad1664d91bda5b
DanicaMaglalang <32055051+DanicaMaglalang@users.noreply.github.com>
2017-09-29 21:59:24 +0800

    Add files via upload

---
+++ PESO Services.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PESOMS</title>
+	<img src="IMAGE/15.jpg" width="100%">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+    <meta name="author" content="Muhammad Usman">
+
+    <!-- The styles -->
+    <link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
+
+    <link href="css/charisma-app.css" rel="stylesheet">
+    <link href='bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
+    <link href='bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
+    <link href='bower_components/chosen/chosen.min.css' rel='stylesheet'>
+    <link href='bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
+    <link href='bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='css/jquery.noty.css' rel='stylesheet'>
+    <link href='css/noty_theme_default.css' rel='stylesheet'>
+    <link href='css/elfinder.min.css' rel='stylesheet'>
+    <link href='css/elfinder.theme.css' rel='stylesheet'>
+    <link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
+    <link href='css/uploadify.css' rel='stylesheet'>
+    <link href='css/animate.min.css' rel='stylesheet'>
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+	  <!-- The fav icon -->
+  <link rel="shortcut icon" href="IMAGE/1.png">
+   
+
+</head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #a80a0a;
+}
+
+li {
+    float: left;
+}
+
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 14px 28px;
+    text-decoration: none;
+
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: white;
+}
+
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #00acc1;
+    min-width: 150px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+.dropdown-content a {
+    color: black1;
+    padding: 14px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #EC2525}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+</style>
+<body>
+<?php
+
+include("configure1.php");
+
+?>
+<ul>
+                 <li><a href="http://localhost/html/index.php" style="text-decoration: none"><font size= "3">Home</a></li>
+                 <li><a href="http://localhost/html/HOMEPAGE/PESO%20Function.php" style="text-decoration: none">P.E.S.O Function</a></li>
+		         <li><a href="http://localhost/html/HOMEPAGE/PESO%20Services.php" style="text-decoration: none">P.E.S.O Services</a></li>
+                 <li><a href="http://localhost/html/HOMEPAGE/contactus.php" style="text-decoration: none">Contact Us</a></li></font>
+
+		
+ 
+</ul>
+<br>
+ <div id="" class="box col-md-12">
+            <!-- content starts -->
+                <div>
+        <ul class="">
+          
+        </ul>
+    </div>
+
+<div class="ch-container">
+    <div class="row">
+
+        <!-- left menu starts -->
+       
+	
+<div id="content" class="col-lg-12 col-sm-12">
+            <!-- content starts -->
+<div> 
+</div>
+ <div class="row" >
+        <div class="col-sm-3 col-lg-9">
+            <div class="box-inner">
+                <div class="box-header well" data-original-title="">
+					<h2>SERVICES </h2>
+                </div>
+               <div class="box-content">
+                 <table class="table table-striped ">
+                        <thead>
+<center><font face="Courier" size="4" color="black"><b><u>  Assistance in the Conduct of Local Recruitment Activity
+(LRA) - for local employment </b></u></center></font>
+<br>
+<b><font face="Courier" size="3" color="black">   ABOUT THE SERVICE: </b> 
+<bgcolor="white"><font face="Courier" size="4" color="black"> <br><br> &nbsp&nbsp&nbsp&nbsp Local establishments, sub-contractor
+agencies, or companies may also seek the assistance of PESO-San Antonio in conducting Local Recruitment Activity.
+<bgcolor="white"><font face="Courier" size="4" color="black">   They may use the facilities and 
+equipment of the office free of charge and may request for staff assistance.
+<bgcolor="white"><font face="Courier" size="4" color="black">   Local employers may leave their 
+job orders with complete job description to PESO-San Antonio for job matching.
+<bgcolor="white" ><font face="Courier" size="4" color="black"><b><br><br> &nbsp&nbsp&nbsp&nbsp REQUIREMENTS : </b>
+<bgcolor="white" ><font face="Courier" size="4" color="black"><b><br><br> &nbsp&nbsp&nbsp&nbsp For the recruitment agency/employer </b>
+<bgcolor="white" ><font face="Courier" size="4" color="black"><br><br> &nbsp&nbsp&nbsp&nbsp --> Confirmed job orders of the company,
+establishment, etc. <br>
+&nbsp&nbsp&nbsp&nbsp --> Company Profile</b>
+ <bgcolor="white" ><font face="Courier" size="4" color="black"><b><br><br> &nbsp&nbsp&nbsp&nbsp For the applicants</b>
+ <bgcolor="white" ><font face="Courier" size="4" color="black"><br><br> &nbsp&nbsp&nbsp&nbsp --> Resume (to include complete job 
+ description) with picture
+<br> &nbsp&nbsp&nbsp&nbsp --> Barangay Certification
+<br> &nbsp&nbsp&nbsp&nbsp --> Employment/Training Certificate
+<br> &nbsp&nbsp&nbsp&nbsp --> Photocopy of WQT, for welders
+<br> &nbsp&nbsp&nbsp&nbsp --> Whole body picture (formal attire), for waiters/waitresses
+</b>
+
+
+<br><br><br>
+<hr>
+<center><font face="Courier" size="4" color="black"><b><u>  Assistance in the Conduct of Special Recruitment Activity
+(SRA) - for overseas employment </b></u></center>
+<br>
+
+<b><font face="Courier" size="3" color="black">  ABOUT THE SERVICE: </b> 
+<br><br>
+<bgcolor="white"><font face="Courier" size="4" color="black">  &nbsp&nbsp&nbsp&nbsp All land licensed recruitment agency
+may seek PESO-San Antonio assistance in conducting special recruitment activities in the municipality.
+<br><br>
+<bgcolor="white"><font face="Courier" size="4" color="black">  &nbsp&nbsp&nbsp&nbsp PESO-San Antonio makes sure that the agency 
+seeking assistance is authorized by the Philippine Overseas Employment Administration (POEA) 
+and shall in no way allow any land based licensed recruitment agency with incomplete
+documents to conduct Special Recruitment Activity (SRA).
+<br><br>
+<bgcolor="white"><font face="Courier" size="4" color="black">  &nbsp&nbsp&nbsp&nbsp They may use the facilities and equipment of the office and may request for staff support.</tr></td></font>
+<bgcolor="white" ><font face="Courier" size="4" color="black">  This service is offered FREE OF CHARGE. </b>
+<br><br>
+<bgcolor="white" ><font face="Courier" size="4" color="black"><b> &nbsp&nbsp&nbsp&nbsp REQUIREMENTS :</b>
+<br><br>
+<bgcolor="white" ><font face="Courier" size="4" color="black"><b> &nbsp&nbsp&nbsp&nbsp For the recruitment agency/employer </b>
+<br><br>
+<bgcolor="white" ><font face="Courier" size="4" color="black"> &nbsp&nbsp&nbsp&nbsp --> Letter of intent and copies of Accreditation Certification
+ <br>&nbsp&nbsp&nbsp&nbsp  -->  Confirmed Job Orders from principal employers abroad 
+ <br><br>&nbsp&nbsp&nbsp&nbsp Both must beduly approved by the Philippine Overseas Employment Administration (POEA).</b>
+ <br><br>
+ <bgcolor="white" ><font face="Courier" size="4" color="black"><b> &nbsp&nbsp&nbsp&nbsp For the applicants</b>
+ <br><br>
+ <bgcolor="white" ><font face="Courier" size="4" color="black"> &nbsp&nbsp&nbsp&nbsp --> Resume (to include complete job 
+ description) with picture
+<br> &nbsp&nbsp&nbsp&nbsp --> NSO Authenticated Birth Certificate
+<br> &nbsp&nbsp&nbsp&nbsp --> NBI Clearance for abroad
+<br> &nbsp&nbsp&nbsp&nbsp --> Employment/Training Certificate
+<br> &nbsp&nbsp&nbsp&nbsp --> Valid Passport
+<br> &nbsp&nbsp&nbsp&nbsp --> Photocopy of WQT, for welders
+<br> &nbsp&nbsp&nbsp&nbsp --> Whole body picture (formal attire), for waiters/waitresses
+</font></b></font></font></font></font></font>
+             </table>
+                
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-3 col-lg-3">
+            <div class="box-inner">
+                <div class="box-header well" data-original-title="">
+                    <h2>FOR APPLICANTS ONLY</h2>
+                </div>
+                <div class="box-content">
+                    <table class="table">
+                        <thead>
+						  <div class="alert alert-info">
+							Please login with your Username and Password.
+						</div>
+            <form class="form-horizontal" action="pasukan.php" method="Get">
+                <fieldset>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user red"></i></span>
+                        <input type="text" class="form-control" placeholder="Username" name="uname" required>
+                    </div>
+                    <div class="clearfix"></div><br>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock red"></i></span>
+                        <input type="password" class="form-control" placeholder="Password" name="psw" required>
+                    </div>
+					<br>
+                    <p class="center col-md-8">
+                        <button type="submit" class="btn btn-danger">Login</button>
+                    </p>
+				</fieldset>
+            </form>
+			</table>
+        </div>
+        <!--/span-->
+    </div><!--/row-->
+</div><!--/fluid-row-->
+<div class="row">
+        <div class="box col-md-3">
+            <div class="box-inner">
+                <div class="box-header well" data-original-title="">
+                    <h2>FOR EMPLOYERS ONLY</h2>
+                </div>
+                <div class="box-content">
+					 <div class="alert alert-info">
+								Please login with your Username and Password.
+					 </div>
+                    <table class="table">
+                        <thead>
+							<form action="employerpage/employpasukan.php" method="Get">
+								<fieldset>
+								<div class="input-group input-group-lg">
+									<span class="input-group-addon"><i class=" glyphicon glyphicon-envelope blue"></i></span>
+									<input type="text" class="form-control" placeholder="Username" name="user" required>
+								</div>
+								<div class="clearfix"></div><br>
+								<div class="input-group input-group-lg">
+									<span class="input-group-addon"><i class="glyphicon glyphicon-lock blue"></i></span>
+									<input type="password" class="form-control" placeholder="Password" name="pass" required>
+								</div>
+								<br>
+								<p class="center col-md-8">
+									<button type="submit" class="btn btn-primary">Login</button>
+								</p>
+								<p> Don't have an account ? <br> <a href="http://localhost/html/HOMEPAGE/employerpage/employersregister.php"> Register here! </a></p>
+								</fieldset>
+							</form>
+						</thead>
+					</table>
+			</div>
+		</div>
+	</div></div></div>
+
+        </div>   </div>
+</div>
+<hr/>
+
+	<footer >
+     <p class="col-md-9 col-sm-9 col-xs-12 copyright"> &copy; Copyright 2017-2018 <br>
+	  San Antonio Public Employment Service Office Management System  <br>
+	  For thesis purposes only <br>
+		Designed by : TeamJOB</p>
+    </footer>
+
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- library for cookie management -->
+<script src="js/jquery.cookie.js"></script>
+<!-- calender plugin -->
+<script src='bower_components/moment/min/moment.min.js'></script>
+<script src='bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
+<!-- data table plugin -->
+<script src='js/jquery.dataTables.min.js'></script>
+
+<!-- select or dropdown enhancer -->
+<script src="bower_components/chosen/chosen.jquery.min.js"></script>
+<!-- plugin for gallery image view -->
+<script src="bower_components/colorbox/jquery.colorbox-min.js"></script>
+<!-- notification plugin -->
+<script src="js/jquery.noty.js"></script>
+<!-- library for making tables responsive -->
+<script src="bower_components/responsive-tables/responsive-tables.js"></script>
+<!-- tour plugin -->
+<script src="bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
+<!-- star rating plugin -->
+<script src="js/jquery.raty.min.js"></script>
+<!-- for iOS style toggle switch -->
+<script src="js/jquery.iphone.toggle.js"></script>
+<!-- autogrowing textarea plugin -->
+<script src="js/jquery.autogrow-textarea.js"></script>
+<!-- multiple file upload plugin -->
+<script src="js/jquery.uploadify-3.1.min.js"></script>
+<!-- history.js for cross-browser state change on ajax -->
+<script src="js/jquery.history.js"></script>
+<!-- application script for Charisma demo -->
+<script src="js/charisma.js"></script>
+		
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PESOMS</title>
+	<img src="IMAGE/15.jpg" width="100%">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+    <meta name="author" content="Muhammad Usman">
+
+    <!-- The styles -->
+    <link id="bs-css" href="css/bootstrap-cerulean.min.css" rel="stylesheet">
+
+    <link href="css/charisma-app.css" rel="stylesheet">
+    <link href='bower_components/fullcalendar/dist/fullcalendar.css' rel='stylesheet'>
+    <link href='bower_components/fullcalendar/dist/fullcalendar.print.css' rel='stylesheet' media='print'>
+    <link href='bower_components/chosen/chosen.min.css' rel='stylesheet'>
+    <link href='bower_components/colorbox/example3/colorbox.css' rel='stylesheet'>
+    <link href='bower_components/responsive-tables/responsive-tables.css' rel='stylesheet'>
+    <link href='bower_components/bootstrap-tour/build/css/bootstrap-tour.min.css' rel='stylesheet'>
+    <link href='css/jquery.noty.css' rel='stylesheet'>
+    <link href='css/noty_theme_default.css' rel='stylesheet'>
+    <link href='css/elfinder.min.css' rel='stylesheet'>
+    <link href='css/elfinder.theme.css' rel='stylesheet'>
+    <link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
+    <link href='css/uploadify.css' rel='stylesheet'>
+    <link href='css/animate.min.css' rel='stylesheet'>
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+  <!-- The fav icon -->
+  <link rel="shortcut icon" href="IMAGE/1.png">
+      
+</head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #a80a0a;
+}
+
+li {
+    float: left;
+}
+
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 14px 28px;
+    text-decoration: none;
+
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: white;
+}
+
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #00acc1;
+    min-width: 150px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+.dropdown-content a {
+    color: black1;
+    padding: 14px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #EC2525}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+</style>
+<body>
+<?php
+
+include("configure1.php");
+
+?>
+<ul>
+                 <li><a href="http://localhost/html/index.php" style="text-decoration: none"><font size= "3">Home</a></li>
+                 <li><a href="http://localhost/html/HOMEPAGE/PESO%20Function.php" style="text-decoration: none">P.E.S.O Function</a></li>
+		         <li><a href="http://localhost/html/HOMEPAGE/PESO%20Services.php" style="text-decoration: none">P.E.S.O Services</a></li>
+                 <li><a href="http://localhost/html/HOMEPAGE/contactus.php" style="text-decoration: none">Contact Us</a></li></font>
+
+		
+ 
+</ul>
+<br>
+ <div id="" class="box col-md-12">
+            <!-- content starts -->
+                <div>
+        <ul class="">
+          
+        </ul>
+    </div>
+
+<div class="ch-container">
+    <div class="row">
+
+        <!-- left menu starts -->
+       
+	
+<div id="content" class="col-lg-12 col-sm-12">
+            <!-- content starts -->
+<div> 
+</div>
+		<div class="row" >
+        <div class="col-sm-3 col-lg-5">
+            <div class="box-inner">
+                <div class="box-header well" data-original-title="">
+                    <h2></h2>
+
+                    <div class="box-icon">
+                        
+                    </div>
+                </div>
+                <div class="box-content">
+                    <table class="table">
+					
+					<img src="IMAGE/mission1.jpg" width="100%" height="100%">
+					<br><hr/>
+					<img src="IMAGE/vission1.jpg" width="100%" height="100%">
+
+					 </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3 col-lg-7">
+            <div class="box-inner">
+                <div class="box-header well" data-original-title="">
+                    <h2>CONTACT US</h2>
+
+                    <div class="box-icon">
+                        
+                    </div>
+                </div>
+                <div class="box-content">
+                    <table class="table">
+                        <thead>
+                       
+                        </thead>
+<iframe width="715" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com.ph/maps/place/Municipality+of+San+Antonio,+San+Antonio+Municipal+Hall,+Hererra+Street,+San+Antonio,+3108+Nueva+Ecija/@15.3063614,120.8573449,16z/data=!4m2!3m1!1s0x3396dfce6188891b:0x7c7dcd107ad2a5d7" style="border: 1px solid black">View the <a href="http://openstreetmap.org.ph/map/all+of+philipppines">Map of All of Philipppines</a> or visit <a href="http://openstreetmap.org.ph/">Philippines Open Street Map</a></iframe>
+<hr/>
+                       <tr> <i class=" glyphicon glyphicon-envelope red"></i>
+					   <font face="Courier" size="4" color="black"> EMAIL </font> 
+						</tr><br><br>
+						<tr>
+						<font face="Courier" size="4" color="blue"> info@peso.com </font>
+					    </tr>
+						<br><br>
+						<tr> <i class=" glyphicon glyphicon-earphone red"></i>
+						<font face="Courier" size="4" color="black">TELEPHONE </font>
+					    </tr><br><br>
+						<tr>
+						<font face="Courier" size="4" color="blue">988-4242 </font>
+					    </tr><br><br>
+						<tr> <i class=" glyphicon glyphicon-map-marker red"></i>
+						<font face="Courier" size="4" color="black"> ADDRESS </font>
+					    </tr><br><br>
+						<tr>
+						<font face="Courier" size="4" color="blue"> 2nd floor, City Hall Compound</font>
+					    </tr><br>
+						<tr>
+						<font face="Courier" size="4" color="blue"> San Antonio, Nueva Ecija</font>
+					    </tr>
+                        </thead>
+						</font></font></font>
+                    </table>
+                </div>
+                </div>
+				
+            </div>
+        </div>
+        <!--/span-->
+</div></div></div>
+
+<hr/>
+
+	<footer >
+     <p class="col-md-9 col-sm-9 col-xs-12 copyright"> &copy; Copyright 2017-2018 <br>
+	  San Antonio Public Employment Service Office Management System  <br>
+	  For thesis purposes only <br>
+		Designed by : TeamJOB</p>
+    </footer>
+    
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- library for cookie management -->
+<script src="js/jquery.cookie.js"></script>
+<!-- calender plugin -->
+<script src='bower_components/moment/min/moment.min.js'></script>
+<script src='bower_components/fullcalendar/dist/fullcalendar.min.js'></script>
+<!-- data table plugin -->
+<script src='js/jquery.dataTables.min.js'></script>
+
+<!-- select or dropdown enhancer -->
+<script src="bower_components/chosen/chosen.jquery.min.js"></script>
+<!-- plugin for gallery image view -->
+<script src="bower_components/colorbox/jquery.colorbox-min.js"></script>
+<!-- notification plugin -->
+<script src="js/jquery.noty.js"></script>
+<!-- library for making tables responsive -->
+<script src="bower_components/responsive-tables/responsive-tables.js"></script>
+<!-- tour plugin -->
+<script src="bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js"></script>
+<!-- star rating plugin -->
+<script src="js/jquery.raty.min.js"></script>
+<!-- for iOS style toggle switch -->
+<script src="js/jquery.iphone.toggle.js"></script>
+<!-- autogrowing textarea plugin -->
+<script src="js/jquery.autogrow-textarea.js"></script>
+<!-- multiple file upload plugin -->
+<script src="js/jquery.uploadify-3.1.min.js"></script>
+<!-- history.js for cross-browser state change on ajax -->
+<script src="js/jquery.history.js"></script>
+<!-- application script for Charisma demo -->
+<script src="js/charisma.js"></script>
+	
+</body>
+</html>
